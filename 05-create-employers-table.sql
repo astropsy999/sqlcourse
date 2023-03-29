888e54482a27739bc41ea848e5bc90327ec8c755
astropsy999 <bareust@gmail.com>
2023-03-29 08:33:15 +0300

    created databases and tables, types of data

---
+++ 05-create-employers-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers (
+    company_name VARCHAR(250),
+    company_address VARCHAR(300),
+    -- yearly_revenue FLOAT(5,2) --123.45, 12.1 Note allowed: 1234.56, 1.123
+    yearly_revenue NUMERIC(5,2), -- value, Allowed: 123.12, Not allowed: 12345.67
+    is_hiring BOOLEAN
+);
